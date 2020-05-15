ef39b6c6685f9ea6afe71ce063c74f12123f67e0
Aaron <30347848+aaronmh0305@users.noreply.github.com>
2020-05-14 21:03:45 -0400

    Update JavaBotGUI.java

--- JavaBot/src/main/java/com/JavaBot/demo/JavaBotGUI.java
+++ JavaBot/src/main/java/com/JavaBot/demo/JavaBotGUI.java
@@ -1,19 +1,15 @@
 package com.JavaBot.demo;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.SystemColor;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.border.MatteBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.SoftBevelBorder;
-import javax.swing.JTextField;
-import java.awt.SystemColor;
 import javax.swing.JTextPane;
-import java.awt.Font;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
 
 /**
  * The GUI of the JavaBot application. This displays the chatbot including the
@@ -22,17 +18,18 @@ import java.awt.Font;
  * @author Austin Torres, Aaron Hernandez
  *
  */
+@SuppressWarnings("serial")
 public class JavaBotGUI extends JFrame {
 
 	private Color bgcolor = new Color(195, 246, 247);
-	private JTextField textField;
 
 	public JavaBotGUI() {
-		this.setTitle("JavaBot");
+		super("JavaBot");
 		
 		// Click X to close
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(800, 600);
+		this.setResizable(false);
 		
 		// Set background with getContentPane
 		this.getContentPane().setBackground(bgcolor);
@@ -40,7 +37,6 @@ public class JavaBotGUI extends JFrame {
 		// absolute positioning
 		this.getContentPane().setLayout(null);
 		
-		//JPanel panel = new JPanel();
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.RAISED, SystemColor.activeCaption, SystemColor.textHighlight));
 		scrollPane.setBounds(334, 47, 322, 368);
@@ -61,15 +57,14 @@ public class JavaBotGUI extends JFrame {
 		txtpnJavabot.setBackground(new Color(255, 255, 204));
 		txtpnJavabot.setFont(new Font("Corbel", Font.PLAIN, 42));
 		txtpnJavabot.setText("JavaBot");
+		txtpnJavabot.setEditable(false);
 		txtpnJavabot.setBounds(53, 82, 150, 66);
 		getContentPane().add(txtpnJavabot);
-
 	
 		
 		// Align window center of screen
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
-		
 		
 	}
 
